0cbf3aa90d5eb8f1dd767052f128e243a941a6ac
Mark Pietrusinski <mark.pietrusinski@sqills.com>
2016-11-18 20:02:42 +0100

    made OrderID always unique to prevent duplicate errors if multiple environments share the same icepay account

--- Action/ConvertPaymentAction.php
+++ Action/ConvertPaymentAction.php
@@ -31,7 +31,7 @@ class ConvertPaymentAction implements ActionInterface
                 'Description' => $payment->getDescription(),
                 'Issuer' => $payment->getIssuer(),
                 'Language' => $payment->getLanguage(),
-                'OrderID' => $payment->getOrder()->getId(),
+                'OrderID' => uniqid($payment->getOrder()->getId() . '_'),
                 'Paymentmethod' => $payment->getMethod(),
                 'Reference' => $payment->getId(),
             ]
